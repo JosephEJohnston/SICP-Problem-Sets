128d4bcfdb74040dd42698c63d798f64f5873441
wangzeyuan <wangzeyuan@nowcoder.com>
2022-01-27 12:09:59 +0800

    3.18，3.19，抄答案

--- ch3/3.17~3.19.rkt
+++ ch3/3.17~3.19.rkt
@@ -2,12 +2,15 @@
 
 ; 3.16，仅供错误示例
 
+#|
 (define (count-pairs x)
   (if (not (pair? x))
       0
       (+ (count-pairs (car x))
          (count-pairs (cdr x))
          1)))
+|#
+
 
 #|
 ; 3:
@@ -38,6 +41,45 @@
 |#
 
 ; 3.17, 2022/01/26
-;(define (count-pairs x)
-; (x))
+; 下面几题都不会，看答案了。
+#|
+我们可以通过维持一个记录列表,然后遍历给定的序对结构,
+每当遇到一个序对时,判断它是否已经存在于记录列表,
+如果不存在就将它加进记录列表,并继续遍历这个序对的 car 和 cdr 部分,
+当给定的序对结构遍历完之后,记录列表的长度就是序对的真正个数。
 
+inner 的定义中使用了内置函数 memq ,用于检查给定序对是否存在于记录列表内。
+|#
+(define (count-pairs x)
+    (length (inner x '())))
+
+(define (inner x memo-list)
+    (if (and (pair? x)
+             (eq? false (memq x memo-list)))
+        (inner (car x)
+               (inner (cdr x)
+                      (cons x memo-list)))
+        memo-list))
+
+; 3.18, 3.19, 2022/01/27
+; 快慢指针
+(define (loop? lst)
+    (define (iter x y)
+        (let ((x-walk (list-walk 1 x))
+              (y-walk (list-walk 2 y)))
+            (cond ((or (null? x-walk) (null? y-walk))
+                    #f)
+                  ((eq? x-walk y-walk)
+                    #t)
+                  (else
+                    (iter x-walk y-walk)))))
+    (iter lst lst))
+
+(define (list-walk step lst)
+    (cond ((null? lst)
+            '())
+          ((= step 0)
+            lst)
+          (else
+            (list-walk (- step 1)
+                       (cdr lst)))))
