b0d1c8ad40c507fb2e3cb2122db8ab3da198eb9c
wangzeyuan <wangzeyuan@nowcoder.com>
2022-01-27 19:29:11 +0800

    3.23 暂不实现

--- ch3/3.21~3.23.rkt
+++ ch3/3.21~3.23.rkt
@@ -76,4 +76,63 @@ delete-queue! 操作仅仅删除队列首个元素，并没有对队尾指针进
 ((q1 'print-queue))
 |#
 
+; 3.23, 2022/01/27
+; 实现双端队列
+; 需要双向链表
+(define (make-deque)
+  (let ((deque '())
+        (front-ptr '())
+        (rear-ptr '()))
+    
+    (define (empty-deque?)
+      (null? deque))
+    
+    (define (front-deque)
+      (car front-ptr))
+
+    (define (rear-deque)
+      (car rear-ptr))
+
+    (define (front-insert-deque! item)
+      (let ((new-pair (cons item '())))
+        (cond ((empty-deque?)
+               (set! deque new-pair)
+               (set! front-ptr new-pair)
+               (set! rear-ptr new-pair))
+              (else
+               (set! deque (cons item deque))
+               (set! front-ptr new-pair)))))
+
+    (define (rear-insert-deque! item)
+      (let ((new-pair (cons item '())))
+        (cond ((empty-deque?)
+               (set! deque new-pair)
+               (set! front-ptr new-pair)
+               (set! rear-ptr new-pair))
+              (else
+               (set-cdr! rear-ptr new-pair)
+               (set! rear-ptr new-pair)))))
+
+    (define (front-delete-deque!)
+      (cond ((empty-deque?)
+             (error "DELETE! called with an empty queue"))
+            (else
+             (set! front-ptr (cons (cdr deque) '()))
+             (set! deque (cdr deque)))))
+
+    ; 需要有一个指针总是指向 rear-ptr 的前一个元素
+    (define (rear-delete-deque!)
+      ())
+    
+    (define (dispatch m)
+      (cond ((eq? m 'empty-deque?) empty-deque?)
+            ((eq? m 'front-deque) front-deque)
+            ((eq? m 'rear-deque) rear-deque)
+            ((eq? m 'front-insert-deque!) front-insert-deque!)
+            ((eq? m 'rear-insert-deque!) rear-insert-deque!)
+            ((eq? m 'front-delete-deque!) front-delete-deque!)
+            ((eq? m 'rear-delete-deque!) rear-delete-deque!)
+            (else (error "Undefined operation -- MAKE-DEQUE" m))))
+
+    dispatch))
 
