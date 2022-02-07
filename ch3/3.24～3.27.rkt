a1d7a3f1402cc71cd96585d11115f346f4b06524
wangzeyuan <wangzeyuan@nowcoder.com>
2022-02-07 11:02:57 +0800

    3.27

--- ch3/3.24～3.27.rkt
+++ ch3/3.24～3.27.rkt
@@ -196,7 +196,8 @@
 
 
 
-;二叉树测试
+; 二叉树测试
+#|
 (define test-tree int-tree)
 ((test-tree 'insert-proc!) 2)
 ((test-tree 'insert-proc!) 1)
@@ -210,53 +211,41 @@
 ((test-tree 'lookup-proc!) 4)
 
 ((test-tree 'print-proc))
+|#
+
+; 可以在父表格和子表格里使用二叉树，需要提供相应的排序方法
+
+; 3.27, 2022/02/07
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+#|
+; 这只是个伪代码,我们的表格是二维的
+(define (memoize f)
+  (let ((table (make-table)))
+    (lambda (x)
+      (let ((previously-computed-result (lookup x table)))
+        ; 当 previously-computed-result 为 false 时,向 table 插入新值
+        (or previously-computed-result
+            (let ((result (f x)))
+              (insert! x result table)
+              result))))))
+
+(define memo-fib  
+  (memoize (lambda (n)
+             (cond ((= n 0) 0)
+                   ((= n 1) 1)
+                   (else (+ (memo-fib (- n 1))
+                            (memo-fib (- n 2))))))))
+
+|#
+
+; 因为不需要计算很多重叠的子问题；对于第 n 个数，之前已经有计算结果，计算一次即可
+; 不能工作，fib 函数依然会计算那些子问题
 
 
-(define (make-tree-table same-key?)
-  (let ((local-table (list '*table*)))
-
-    ; 查找 records 中是否存在对应的 key
-    (define (assoc key records)
-      (cond ((null? records) false)
-        ((same-key? key (caar records)) (car records))
-        (else (assoc key (cdr records)))))
-    
-    ; 用 key-1 查找子表格,用 key-2 在这个子表格里确定记录
-    (define (lookup key-1 key-2)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (cdr record)
-                  false))
-            false)))
-
-    ; 在 key-1 子表格中,插入名为 key-2 的记录
-    (define (insert! key-1 key-2 value)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            ; 存在子表,则插入旧表
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (set-cdr! record value)
-                  (set-cdr! subtable
-                            (cons (cons key-2 value)
-                                  (cdr subtable)))))
-            ; 不存在,初始化并插入新子表和值
-            (set-cdr! local-table
-                      (cons (list key-1
-                                  (cons key-2 value))
-                            (cdr local-table)))))
-      'ok)
-
-    (define (print)
-      local-table)
-
-    (define (dispatch m)
-      (cond ((eq? m 'lookup-proc) lookup)
-            ((eq? m 'insert-proc!) insert!)
-            ((eq? m 'print-proc) print)
-            (else (error "Unknown operation -- TABLE" m))))
-
-    dispatch))
 
