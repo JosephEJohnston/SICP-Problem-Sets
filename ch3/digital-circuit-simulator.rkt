f4f8e4de3c02359ed4659e10b2805b2e12ea3ad8
wangzeyuan <wangzeyuan@nowcoder.com>
2022-02-08 11:36:51 +0800

    模拟器代码

--- ch3/digital-circuit-simulator.rkt
+++ ch3/digital-circuit-simulator.rkt
@@ -18,8 +18,8 @@
 
     (define (dispatch m)
       (cond ((eq? m 'get-signal) signal-value)
-            ((eq? m 'set-signal) set-my-signal!)
-            ((eq? m 'add-action) accept-action-procedure!)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
             (else (error "Unknown operation -- WIRE" m))))
     dispatch))
 
@@ -182,3 +182,123 @@
         (first-item)
         (remove-first-agenda-item! the-agenda)
         (propagate))))
+
+
+;; 实例模拟
+
+; 线路上的“监测器”，用于显示模拟器的活动
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display " New-value = ")
+                 (display (get-signal wire)))))
+
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+; 添加“监测器”
+(probe 'sum sum)
+(probe 'carry carry)
+
+; 将线路连接到一个半加器电路上，执行模拟
+
+; 逻辑否
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+; 反门
+(define (inverter input output)
+  (define (invert-input)
+    ; 对 input 线上取到的信号值取逻辑否
+    (let ((new-value (logical-not (get-signal input))))
+      ; 在 inverter-delay 后,在 output 线上设置新值
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+
+  ; 在 input 线上加上 invert-input 过程
+  (add-action! input invert-input)
+  'ok)
+
+; 逻辑与
+(define (logical-and s1 s2)
+  (cond ((= s1 s2 1) 1)
+        ((not (= s1 s2)) 0)
+        (else 0)))
+
+; 与门
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+; 逻辑或
+(define (logical-or s1 s2)
+  (cond ((= s1 s2 0) 0)
+        (else 1)))
+
+; 或门
+(define (or-gate o1 o2 output)
+  (define (or-action-procedure)
+    (let  ((new-value
+            (logical-or (get-signal o1) (get-signal o2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+
+  (add-action! o1 or-action-procedure)
+  (add-action! o1 or-action-procedure)
+  'ok)
+
+; 半加器
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+; 全加器
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+; 模拟 1
+(half-adder input-1 input-2 sum carry)
+
+(set-signal! input-1 1)
+
+(propagate)
+
+
+; 模拟 2
+(set-signal! input-2 1)
+
+(propagate)
+
+
+
