c787d9bd6752000ff6a591311039dcf1134286b1
wangzeyuan <wangzeyuan@nowcoder.com>
2022-03-12 19:07:50 +0800

    3.81

--- ch3/3.81_3.82.rkt
+++ ch3/3.81_3.82.rkt
@@ -4,3 +4,22 @@
 
 ; 3.81, 2022/03/11, 实现随机数生成器的流模型
 ; 解中不要使用赋值
+
+(define (rand-generator stream)
+
+  (define random-numbers
+    (cons-stream (rand-update (stream-car stream))
+                 (stream-map rand-update (stream-cdr stream))))
+
+  (define (generate-rand)
+    (stream-car random-numbers
+    
+  
+  (define (dispatch m)
+    (cond ((eq? m 'generate-rand) generate-rand)
+          ((eq? m 'reset-rand) reset-rand)
+          (else (error "Unknown Operation Error -- rand-generator")))))
+
+
+           
+
---
+++ ch3/3.81_3.82.rkt~
@@ -0,0 +1,6 @@
+#lang sicp
+
+(#%require "monte-carlo-stream.rkt")
+
+; 3.81, 2022/03/11, 实现随机数生成器的流模型
+; 解中不要使用赋值
